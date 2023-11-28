e9fc7eacc4cfbbaa7eb559b3393e3342acd016bb
Abdul <you@example.com>
2023-11-28 09:27:54 +0100

    Services-section-done

--- src/app/services-section/services-section.component.ts
+++ src/app/services-section/services-section.component.ts
@@ -10,33 +10,33 @@ export class ServicesSectionComponent {
   services = [
     {
       title: 'Complete renovation',
-      description: 'Lorem ipsum dolor sit amet...',
-      image: '../../assets/img/Icon_mobile.png'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/img/service-images/service-1.png'
+    },
+    {
+      title: 'Kücheneinbau',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/img/service-images/service2.png'
+    },
+    {
+      title: 'Bad Renovierung',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/img/service-images/service3.png'
     },
     {
       title: 'Complete renovation',
-      description: 'Lorem ipsum dolor sit amet...',
-      image: '../../assets/img/Icon_mobile.png'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/img/service-images/service-1.png'
     },
     {
       title: 'Complete renovation',
-      description: 'Lorem ipsum dolor sit amet...',
-      image: '../../assets/img/Icon_mobile.png'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/img/service-images/service2.png'
     },
     {
       title: 'Complete renovation',
-      description: 'Lorem ipsum dolor sit amet...',
-      image: '../../assets/img/Icon_mobile.png'
-    },
-    {
-      title: 'Complete renovation',
-      description: 'Lorem ipsum dolor sit amet...',
-      image: '../../assets/img/Icon_mobile.png'
-    },
-    {
-      title: 'Complete renovation',
-      description: 'Lorem ipsum dolor sit amet...',
-      image: '../../assets/img/Icon_mobile.png'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/img/service-images/service3.png'
     },
     // ... Weitere Dienste
   ];
